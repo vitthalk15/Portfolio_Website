8a126125381cbffc72d5ee4ad8edb38dd25e49bb
Vitthal <vitthal@kali.Kali>
2024-04-18 23:08:57 +0530

    Initial commit

---
+++ components/shared/AppFooterCopyright.jsx
@@ -0,0 +1,26 @@
+function AppFooterCopyright() {
+	return (
+		<div className="font-general-regular flex justify-center items-center text-center">
+			<div className="text-lg text-ternary-dark dark:text-ternary-light">
+				&copy; {new Date().getFullYear()}
+				<a
+					href="https://github.com/vitthalk15"
+					target="__blank"
+					className="hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
+				>
+					Next.js & Tailwind CSS Portfolio
+				</a>
+				.{' '}
+				<a
+					href="https://vitthalk15.me"
+					target="__blank"
+					className="text-secondary-dark dark:text-secondary-light font-medium uppercase hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
+				>
+					VK
+				</a>
+			</div>
+		</div>
+	);
+}
+
+export default AppFooterCopyright;
---
+++ data/aboutMeData.js
@@ -0,0 +1,8 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export const aboutMeData = [
+	{
+		id: uuidv4(),
+		bio: 'I am Vitthal Kendre , B.Tech CSE student at Indian Instutute of Information Technology Vadodara - International Campus Diu.',
+	},
+];
